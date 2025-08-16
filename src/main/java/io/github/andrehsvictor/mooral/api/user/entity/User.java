86f34b4a892ecc7a57572a53579398c840c53578
André Victor <andrehsvictor@gmail.com>
2025-08-16 15:48:49 -0300

    feat: refactor User entity and migrate application properties to YAML format

--- src/main/java/io/github/andrehsvictor/mooral/api/user/entity/User.java
+++ src/main/java/io/github/andrehsvictor/mooral/api/user/entity/User.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -22,30 +21,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/*
- * CREATE TABLE IF NOT EXISTS user (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    email_verified BOOLEAN DEFAULT FALSE,
-    bio TEXT,
-    avatar_url VARCHAR(255),
-    password VARCHAR(255),
-    oauth_provider VARCHAR(255),
-    oauth_id VARCHAR(255),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    suspended_until TIMESTAMP WITH TIME ZONE
-);
- */
 @Getter
 @Setter
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
 @EqualsAndHashCode(of = { "id" })
 @ToString(exclude = { "password", "oauthId" })
 public class User implements Serializable {
