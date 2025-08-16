ca607351c4d7b86048aaf258bc34924829521aa1
André Victor <andrehsvictor@gmail.com>
2025-08-16 09:41:12 -0300

    feat: initialize project with Maven, Spring Boot, and necessary configurations

---
+++ src/main/java/io/github/andrehsvictor/mooral/api/MooralApplication.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.mooral.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooralApplication.class, args);
+	}
+
+}
