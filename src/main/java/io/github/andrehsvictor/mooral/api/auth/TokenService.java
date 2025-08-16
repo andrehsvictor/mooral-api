b34fad15ae4aaa3b0c08c5fb7c51b126da2f84d2
André Victor <andrehsvictor@gmail.com>
2025-08-16 18:25:10 -0300

    feat: implement TokenService and add JwtService for JWT handling

--- src/main/java/io/github/andrehsvictor/mooral/api/auth/TokenService.java
+++ src/main/java/io/github/andrehsvictor/mooral/api/auth/TokenService.java
@@ -1,5 +1,13 @@
 package io.github.andrehsvictor.mooral.api.auth;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class TokenService {
+
+    private final AuthenticationService authenticationService;
 
 }
---
+++ src/main/java/io/github/andrehsvictor/mooral/api/shared/jwt/JwtService.java
@@ -0,0 +1,11 @@
+package io.github.andrehsvictor.mooral.api.shared.jwt;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+}
