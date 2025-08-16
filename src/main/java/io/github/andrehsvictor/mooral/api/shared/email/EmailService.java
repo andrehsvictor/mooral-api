ae323b2584991775e12bac2795432c2dfa600a0f
André Victor <andrehsvictor@gmail.com>
2025-08-16 18:23:49 -0300

    feat: add EmailService for handling email-related functionalities

---
+++ src/main/java/io/github/andrehsvictor/mooral/api/shared/email/EmailService.java
@@ -0,0 +1,11 @@
+package io.github.andrehsvictor.mooral.api.shared.email;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+}
