3c969da29edb3936aa7b1677bc7418dc9ecc1162
André Victor <andrehsvictor@gmail.com>
2025-08-16 21:32:50 -0300

    feat: update TokenPairDto to include JSON property annotations and improve field naming

--- src/main/java/io/github/andrehsvictor/mooral/api/shared/dto/auth/TokenPairDto.java
+++ src/main/java/io/github/andrehsvictor/mooral/api/shared/dto/auth/TokenPairDto.java
@@ -1,16 +1,30 @@
 package io.github.andrehsvictor.mooral.api.shared.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TokenPairDto {
+
+    @JsonProperty("access_token")
     private String accessToken;
+
+    @JsonProperty("refresh_token")
     private String refreshToken;
 
     @Builder.Default
-    private String type = "Bearer";
-    
-    private Long expiresIn;
+    @JsonProperty("token_type")
+    private String tokenType = "Bearer";
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    private String scope;
+
+
 }
