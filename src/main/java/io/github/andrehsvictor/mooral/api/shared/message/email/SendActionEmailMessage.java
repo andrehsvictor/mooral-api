6a098609c5d9359df8b38578173b8b0915307c97
André Victor <andrehsvictor@gmail.com>
2025-08-23 10:58:08 -0300

    feat: add SendActionEmailMessage class for email message handling

---
+++ src/main/java/io/github/andrehsvictor/mooral/api/shared/message/email/SendActionEmailMessage.java
@@ -0,0 +1,15 @@
+package io.github.andrehsvictor.mooral.api.shared.message.email;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SendActionEmailMessage {
+
+    private String name;
+    private String email;
+    private String url;
+    private String token;
+    
+}
