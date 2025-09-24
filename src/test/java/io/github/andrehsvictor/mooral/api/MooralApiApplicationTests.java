39412a6a09c41ed0e15179a240bc21e93cd05fad
André Victor <andrehsvictor@gmail.com>
2025-09-23 21:15:46 -0300

    Initialize Maven project with wrapper scripts, configuration files, and basic application structure

---
+++ src/test/java/io/github/andrehsvictor/mooral/api/MooralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.mooral.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
